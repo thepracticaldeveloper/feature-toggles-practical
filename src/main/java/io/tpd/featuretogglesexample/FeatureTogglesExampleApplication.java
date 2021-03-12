56ec6c99fccff9eab0ebaff42ee7f126bb529d13
Moises Macero <mechero@users.noreply.github.com>
2021-03-12 10:02:20 +0100

    Initializr baseline

---
+++ src/main/java/io/tpd/featuretogglesexample/FeatureTogglesExampleApplication.java
@@ -0,0 +1,13 @@
+package io.tpd.featuretogglesexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeatureTogglesExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeatureTogglesExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/tpd/featuretogglesexample/FeatureTogglesExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package io.tpd.featuretogglesexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeatureTogglesExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
